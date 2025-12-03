22c4212d8de7f12da8a2cb68033cb43ec075cdf2
Mark Edward <markbarrera@vakano.studio>
2025-12-03 11:29:05 -0500

    fix - enemy dead animation

--- Assets/scripts/managers/EnemyManager/Enemy.cs
+++ Assets/scripts/managers/EnemyManager/Enemy.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamageable
 {
     #region ══════════ SERIALIZED FIELDS ══════════
     

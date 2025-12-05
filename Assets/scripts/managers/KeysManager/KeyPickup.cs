15564f27d7cb0365301da6185e8fa973ac95bad3
Jeifer Leon <jeiferleon@gmail.com>
2025-12-05 14:07:09 -0500

    refactor - update key pickup logic to use FindFirstObjectByType for player interaction

--- Assets/scripts/managers/KeysManager/KeyPickup.cs
+++ Assets/scripts/managers/KeysManager/KeyPickup.cs
@@ -6,22 +6,22 @@ public class KeyPickup : MonoBehaviour, IInteractable
     [Title("Configuración")]
     [SerializeField] private string _keyId = "key_01";
     [SerializeField] private bool _destroyOnPickup = true;
-    
-    
+
+
     public void Interact()
     {
         // Buscar al jugador
-        PlayerInteraction3D player = FindObjectOfType<PlayerInteraction3D>();
-        
+        PlayerInteraction3D player = FindFirstObjectByType<PlayerInteraction3D>();
+
         if (player != null)
         {
             player.AddKey(_keyId);
-            
+
             if (_destroyOnPickup)
                 Destroy(gameObject);
         }
     }
-    
+
     public string GetInteractionText()
     {
         return $"Recoger llave [{_keyId}]";
